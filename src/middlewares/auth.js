cc4fb346a6109e36e2aaf255965775541b6cfd49
Mohammedar12 <alikkar87@gmail.com>
2023-06-07 19:08:26 +0300

    A

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,75 +1,64 @@
-const config = require('../config/config');
+const config = require("../config/config");
 
-const auth = (roles,allow_if_app = true) => (req, res, next) => {
-    if(req.session?.user?.id){
-        if(!roles || roles === '' || roles === []){
-            req.user = req.session.user;
-            next();
-        }
-        else if(Array.isArray(roles)){
-            if(roles.includes(req.session.user.role)){
-                req.user = req.session.user;
-                next();
+const auth =
+  (roles, allow_if_app = false) =>
+  (req, res, next) => {
+    if (req.session?.user?.id) {
+      if (!roles || roles === "" || roles === []) {
+        req.user = req.session.user;
+        next();
+      } else if (Array.isArray(roles)) {
+        if (roles.includes(req.session.user.role)) {
+          req.user = req.session.user;
+          next();
+        } else {
+          if (allow_if_app) {
+            if (req.headers.token === config.frontendToken) {
+              req.user = req.session.user;
+              next();
             }
-            else{
-                if(allow_if_app){
-                    if(req.headers.token === config.frontendToken){
-                        req.user = req.session.user;
-                        next();
-                    }
-                }
-                else{
-                    res.status(403).send({
-                        status: 403,
-                        message: "Unauthorized request"
-                    });
-                }
-            }
-        }
-        else{
-            if(roles === req.session.user.role){
-                req.user = req.session.user;
-                next();
-            }
-            else{
-                if(allow_if_app){
-                    if(req.headers.token === config.frontendToken){
-                        req.user = req.session.user;
-                        next();
-                    }
-                }
-                else{
-                    console.log("sdfsdf2");
-                    res.status(403).send({
-                        status: 403,
-                        message: "Unauthorized request"
-                    });
-                }
-            }
-        }
-    }
-    else{
-        if(allow_if_app){
-        // if(!allow_if_app){ // for test 
-            if(req.headers.token === config.frontendToken){
-                next();
-            }
-            else{
-                // res.status(401).send({
-                //     status: 401,
-                //     message: "Unauthenticated request"
-                // });
-
-                next();
-            }
-        }
-        else{
-            res.status(401).send({
-                status: 401,
-                message: "Unauthenticated no request"
+          } else {
+            res.status(403).send({
+              status: 403,
+              message: "Unauthorized request",
             });
+          }
         }
+      } else {
+        if (roles === req.session.user.role) {
+          req.user = req.session.user;
+          next();
+        } else {
+          if (allow_if_app) {
+            if (req.headers.token === config.frontendToken) {
+              req.user = req.session.user;
+              next();
+            }
+          } else {
+            console.log("sdfsdf2");
+            res.status(403).send({
+              status: 403,
+              message: "Unauthorized request",
+            });
+          }
+        }
+      }
+    } else {
+      if (allow_if_app) {
+        if (req.headers.token === config.frontendToken) {
+          next();
+        } else {
+          res.status(401).send({
+            status: 401,
+            message: "Unauthenticated request",
+          });
+        }
+      } else {
+        res.status(401).send({
+          status: 401,
+          message: "Unauthenticated request",
+        });
+      }
     }
-};
+  };
 module.exports = auth;
-
